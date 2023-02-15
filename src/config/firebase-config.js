294b5632b0f8f9040be135bf762380d134d3a57b
João Conde <joao997conde@gmail.com>
2023-02-15 22:45:44 +0000

    Changed middleware auth config

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const admin = require("firebase-admin");
 
-const serviceAccount = require('./serviceAccount.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
