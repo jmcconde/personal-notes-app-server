d8f26423a4e68f91951c166b8336b9fcdfe510fe
João Conde <joao997conde@gmail.com>
2023-02-15 21:57:59 +0000

    Changed the default port of the server.

--- src/server.js
+++ src/server.js
@@ -14,7 +14,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const PORT = 5000;
 const db = process.env.MONGO_URI;
 
 app.use(middlware.decodeToken);
@@ -83,8 +82,8 @@ app.delete("/api/deleteNote/:id", async (req, res) => {
 const start = async () => {
     try {
         await mongoose.connect(db);
-        app.listen(PORT, () => {
-            console.log(`Server is listening on port ${PORT}`);
+        app.listen(process.env.PORT || 5000, () => {
+            console.log(`Server is listening on port ${process.env.PORT || 5000}`);
         });
     } catch (error) {
         console.error(error);
